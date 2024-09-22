2f73410f15b9f0e424f68be25c9c7f6303a4f8c1
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-09-22 14:29:52 -0300

    feat: adicionado configuração do MongoDB e Mongo Express no Docker Compose

---
+++ src/main/java/br/com/inovatech/powerguard/PowerguardApplication.java
@@ -0,0 +1,13 @@
+package br.com.inovatech.powerguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerguardApplication.class, args);
+	}
+
+}
