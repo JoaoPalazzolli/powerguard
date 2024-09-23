a194a38a7bb5651ed643abf0bd4cd68fd6609cc6
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-09-23 01:44:23 -0300

    feat: adicionar utilitário de mapeamento de objetos com ModelMapper

---
+++ src/main/java/br/com/inovatech/powerguard/utils/Mapper.java
@@ -0,0 +1,18 @@
+package br.com.inovatech.powerguard.utils;
+
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+
+public class Mapper {
+
+    private static final ModelMapper mapper = new ModelMapper();
+
+    public static <O, D> D parseObject(O source, Class<D> destinationType) {
+        return mapper.map(source, destinationType);
+    }
+
+    public static <O, D> List<D> parseListObject(List<O> source, Class<D> destinationType) {
+        return source.stream().map(x -> mapper.map(x, destinationType)).toList();
+    }
+}
