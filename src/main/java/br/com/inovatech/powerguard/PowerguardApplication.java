5d3e6eff33261a7f65f21cee5b28f7be089afd1a
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-09-23 01:43:25 -0300

    feat: habilitar cache e agendamento de tarefas na aplicação

--- src/main/java/br/com/inovatech/powerguard/PowerguardApplication.java
+++ src/main/java/br/com/inovatech/powerguard/PowerguardApplication.java
@@ -2,12 +2,16 @@ package br.com.inovatech.powerguard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableCaching
 @SpringBootApplication
 public class PowerguardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PowerguardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PowerguardApplication.class, args);
+    }
 
 }
