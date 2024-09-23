6d41dcb35555384598afc2bf10fe776c8d34b200
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-09-23 01:47:52 -0300

    feat: expandir a classe Energy para incluir novos atributos de medição

--- src/main/java/br/com/inovatech/powerguard/domains/Energy.java
+++ src/main/java/br/com/inovatech/powerguard/domains/Energy.java
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,5 +22,30 @@ public class Energy implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    private String id;
+    private Integer id;
+
+    private Date timestamp;
+    private Double tensaoa;
+    private Double tensaob;
+    private Double tensaoc;
+    private Double correntea;
+    private Double correnteb;
+    private Double correntec;
+    private Double potativaa;
+    private Double potativab;
+    private Double potativac;
+    private Double potativatotal;
+    private Double potreativaa;
+    private Double potreativab;
+    private Double potreativac;
+    private Double potreativatotal;
+    private Double potaparentea;
+    private Double potaparenteb;
+    private Double potaparentec;
+    private Double potaparentetotal;
+    private Double fatorpotenciaa;
+    private Double fatorpotenciab;
+    private Double fatorpotenciac;
+    private Double fatorpotenciatotal;
+    private Date createdAt;
 }
