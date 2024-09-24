f7e8994d50a4893c5082e7306acf89cb9266ca84
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-09-24 19:49:53 -0300

    refactor: remover ResponseEnergyMonitoringAPI e adicionar mapeamento dinâmico de resposta da API

--- src/main/java/br/com/inovatech/powerguard/infra/external/proxy/EnergyMonitoringAPI.java
+++ src/main/java/br/com/inovatech/powerguard/infra/external/proxy/EnergyMonitoringAPI.java
@@ -1,11 +1,14 @@
 package br.com.inovatech.powerguard.infra.external.proxy;
 
 import br.com.inovatech.powerguard.dtos.EnergyDTO;
-import br.com.inovatech.powerguard.infra.external.domain.ResponseEnergyMonitoringAPI;
+import br.com.inovatech.powerguard.utils.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class EnergyMonitoringAPI {
@@ -15,14 +18,19 @@ public class EnergyMonitoringAPI {
     @Autowired
     private RestTemplate restTemplate;
 
+    @SuppressWarnings("unchecked")
     public Flux<EnergyDTO> getByHour() {
-        var response = restTemplate.getForEntity(API_URL, ResponseEnergyMonitoringAPI.class);
+        var response = restTemplate.getForEntity(API_URL, Map.class).getBody();
 
-        if (response.getBody() == null) {
+        if (response == null) {
             return null;
         }
 
-        return Flux.fromIterable(response.getBody().getResults());
+        var resultList = Mapper.parseObject(response.get("results"), List.class);
+
+        var convetedList = Mapper.parseListObject(resultList, EnergyDTO.class);
+
+        return Flux.fromIterable(convetedList);
     }
 
 }
