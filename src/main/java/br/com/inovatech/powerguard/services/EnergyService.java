b5caebb18f0ee93a03c9e17b4a8b8bd0d94b982b
João Pedro <joaopedropalazzolli@gmail.com>
2024-09-25 22:01:51 -0300

    refactor: substituir injeção de EnergyMonitoringAPI único por lista de APIs no EnergyService

--- src/main/java/br/com/inovatech/powerguard/services/EnergyService.java
+++ src/main/java/br/com/inovatech/powerguard/services/EnergyService.java
@@ -4,7 +4,7 @@ import br.com.inovatech.powerguard.domains.Energy;
 import br.com.inovatech.powerguard.dtos.EnergyDTO;
 import br.com.inovatech.powerguard.infra.external.proxy.EnergyMonitoringAPI;
 import br.com.inovatech.powerguard.repositories.EnergyRepository;
-import br.com.inovatech.powerguard.utils.Mapper;
+import br.com.inovatech.powerguard.infra.utils.Mapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,12 +14,11 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class EnergyService {
-
-    @Autowired
-    private EnergyMonitoringAPI api;
 
     @Autowired
     private ReactiveRedisTemplate<String, Object> redisTemplate;
@@ -27,34 +26,47 @@ public class EnergyService {
     @Autowired
     private EnergyRepository energyRepository;
 
+    @Autowired
+    private List<EnergyMonitoringAPI> energyMonitoringAPI;
+
     @Value("${security.cache_key}")
     private String CACHE_KEY;
 
-    public Mono<Object> findAllByHour() {
+    public EnergyService(List<EnergyMonitoringAPI> energyMonitoringAPI) {
+        this.energyMonitoringAPI = energyMonitoringAPI;
+    }
+
+    public Mono<Object> findAll() {
         return redisTemplate.opsForValue().get(CACHE_KEY);
     }
 
-    @Scheduled(fixedRate = 36000000)
+    @Scheduled(fixedRate = 300000)
     private void refreshEnergyData() {
         log.info("Refreshing Energy Data");
 
-        updateEnergyInDB()
-                .thenMany(energyRepository.findAll().map(energy -> Mapper.parseObject(energy, EnergyDTO.class)))
-                .collectList()
-                .flatMapMany(energyList ->
-                        redisTemplate.opsForValue()
-                                .set(CACHE_KEY, energyList))
-                .subscribe(result -> log.info("Energy data updated and cached successfully"),
-                        error -> log.error("Error during energy data update", error));
+            energyMonitoringAPI.forEach(x -> {
+                updateEnergyInDB(x)
+                        .thenMany(energyRepository.findAll().map(energy -> Mapper.parseObject(energy, EnergyDTO.class)))
+                        .collectList()
+                        .flatMapMany(energyList ->
+                                redisTemplate.opsForValue()
+                                        .set(CACHE_KEY, energyList))
+                        .subscribe(result -> log.info("Energy data updated and cached successfully"),
+                                error -> log.error("Error during energy data update", error));
+            });
+
+
+
+
     }
 
-    private Mono<Void> updateEnergyInDB() {
+    private Mono<Void> updateEnergyInDB(EnergyMonitoringAPI api) {
         log.info("Updating Energy data in Database");
 
         return energyRepository.findAll()
                 .collectList()
                 .flatMap(dbEnergy ->
-                        api.getByHour().filter(apiEnergy ->
+                        api.getEnergyData().filter(apiEnergy ->
                                         dbEnergy.stream().noneMatch(x ->
                                                 apiEnergy.getId().equals(x.getId()))).collectList()
                                 .flatMapMany(energyList ->
@@ -62,7 +74,7 @@ public class EnergyService {
                                                 .flatMap(newEnergy ->
                                                         energyRepository.save(Mapper.parseObject(newEnergy, Energy.class))
                                                                 .doOnSuccess(savedEnergy ->
-                                                                        log.info("Saved new energy data with ID: {}", savedEnergy.getId())
+                                                                        log.info("Saved new energy data with ID: {} in building: {}", savedEnergy.getId(), api.getBuildingName())
                                                                 )
                                                 ).doFinally(x -> Mapper.parseObject(x, EnergyDTO.class))
                                 ).then());
