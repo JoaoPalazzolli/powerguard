263c47a475745de86512f11df17603456dadb9ca
João Pedro <joaopedropalazzolli@gmail.com>
2024-09-25 22:01:01 -0300

    refactor: transformar EnergyMonitoringAPI em interface e adicionar métodos getEnergyData e getBuildingName

--- src/main/java/br/com/inovatech/powerguard/infra/external/proxy/EnergyMonitoringAPI.java
+++ src/main/java/br/com/inovatech/powerguard/infra/external/proxy/EnergyMonitoringAPI.java
@@ -1,36 +1,12 @@
 package br.com.inovatech.powerguard.infra.external.proxy;
 
 import br.com.inovatech.powerguard.dtos.EnergyDTO;
-import br.com.inovatech.powerguard.utils.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Flux;
 
-import java.util.List;
-import java.util.Map;
+public interface EnergyMonitoringAPI {
 
-@Component
-public class EnergyMonitoringAPI {
+    Flux<EnergyDTO> getEnergyData();
 
-    private static final String API_URL = "http://54.208.51.32:30125/api/by-hour/energy_meter_c/";
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @SuppressWarnings("unchecked")
-    public Flux<EnergyDTO> getByHour() {
-        var response = restTemplate.getForEntity(API_URL, Map.class).getBody();
-
-        if (response == null) {
-            return null;
-        }
-
-        var resultList = Mapper.parseObject(response.get("results"), List.class);
-
-        var convetedList = Mapper.parseListObject(resultList, EnergyDTO.class);
-
-        return Flux.fromIterable(convetedList);
-    }
+    String getBuildingName();
 
 }
