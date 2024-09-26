52b7d2678f16e2a8e4f65471f6f7e41e0433394c
João Pedro <joaopedropalazzolli@gmail.com>
2024-09-25 22:04:23 -0300

    refactor: mover classe RedisConfig para o pacote infra.configs

--- src/main/java/br/com/inovatech/powerguard/infra/configs/RedisConfig.java
+++ src/main/java/br/com/inovatech/powerguard/infra/configs/RedisConfig.java
@@ -1,4 +1,4 @@
-package br.com.inovatech.powerguard.configs;
+package br.com.inovatech.powerguard.infra.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
