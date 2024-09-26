77d2da5e6bfc4751fd59722b83c68b07a63619b1
João Pedro <joaopedropalazzolli@gmail.com>
2024-09-25 22:03:59 -0300

    feat: criar classe PageCalculation para calcular a última página com base na contagem

---
+++ src/main/java/br/com/inovatech/powerguard/infra/utils/PageCalculation.java
@@ -0,0 +1,14 @@
+package br.com.inovatech.powerguard.infra.utils;
+
+public class PageCalculation {
+
+    // get the last page
+    public static Integer getLastPage(Integer count){
+
+        if(count == null || count < 0) return  null;
+
+        if(count % 10 != 0) return (int) ((count / 10) + 1);
+
+        return (int) (count / 10);
+    }
+}
