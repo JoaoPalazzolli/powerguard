a4e588c6f160f497f46f7711699b5e6dd6d34150
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-10-08 23:22:11 -0300

    feat: Atualizar controlador de energia para retornar uma lista de EnergyDTO na resposta do endpoint findAll.

--- src/main/java/br/com/inovatech/powerguard/controllers/EnergyController.java
+++ src/main/java/br/com/inovatech/powerguard/controllers/EnergyController.java
@@ -1,11 +1,15 @@
 package br.com.inovatech.powerguard.controllers;
 
+import br.com.inovatech.powerguard.dtos.EnergyDTO;
 import br.com.inovatech.powerguard.services.EnergyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/energy")
@@ -15,7 +19,7 @@ public class EnergyController {
     private EnergyService energyService;
 
     @GetMapping
-    public Mono<Object> findAll() {
+    public Mono<ResponseEntity<List<EnergyDTO>>> findAll() {
         return energyService.findAll();
     }
 }
