bf28607287867724ad295ac74a88662c9611a8de
João Palazzolli <joaopedropalazzolli@gmail.com>
2024-10-08 23:29:16 -0300

    refactor: Adiciona documentação à classe de configuração do Redis reativo

--- src/main/java/br/com/inovatech/powerguard/infra/configs/RedisConfig.java
+++ src/main/java/br/com/inovatech/powerguard/infra/configs/RedisConfig.java
@@ -8,9 +8,25 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+/**
+ * Classe de configuração do Redis reativo.
+ *
+ * Esta classe define o bean para a configuração de um `ReactiveRedisTemplate`, que é usado para
+ * a interação reativa com o banco de dados Redis.
+ *
+ * - O `ReactiveRedisTemplate` é configurado com serializadores para as chaves e valores.
+ * - As chaves são serializadas como strings usando `StringRedisSerializer`.
+ * - Os valores são serializados em JSON utilizando o `Jackson2JsonRedisSerializer`.
+ */
 @Configuration
 public class RedisConfig {
 
+    /**
+     * Configura o `ReactiveRedisTemplate` com serialização para trabalhar com Redis de forma reativa.
+     *
+     * @param factory A fábrica de conexões reativas do Redis.
+     * @return ReactiveRedisTemplate<String, Object> Template reativo configurado para interagir com o Redis.
+     */
     @Bean
     public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
         Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
