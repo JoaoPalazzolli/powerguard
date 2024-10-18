1c4e6d71001c321c5185d6a9a90359d7bc7d7417
João Pedro <joaopedropalazzolli@gmail.com>
2024-10-18 15:19:28 -0300

    feat: Adicionar documentação OpenAPI à operação de signin no AuthController

--- src/main/java/br/com/inovatech/powerguard/controllers/AuthController.java
+++ src/main/java/br/com/inovatech/powerguard/controllers/AuthController.java
@@ -3,6 +3,10 @@ package br.com.inovatech.powerguard.controllers;
 import br.com.inovatech.powerguard.dtos.SigninDTO;
 import br.com.inovatech.powerguard.dtos.TokenDTO;
 import br.com.inovatech.powerguard.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +19,11 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Operation(summary = "Authenticates a user and returns a token", tags = { "Auth" }, responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = TokenDTO.class))),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
     @PostMapping(value = "/signin")
     public Mono<ResponseEntity<TokenDTO>> signin(@RequestBody SigninDTO signinDTO){
         return authService.signin(signinDTO);
