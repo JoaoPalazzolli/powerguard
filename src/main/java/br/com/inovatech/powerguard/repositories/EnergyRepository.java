e5e3a66c3ef5339c7d356561994efd4b2b45534a
João Pedro <joaopedropalazzolli@gmail.com>
2024-10-18 21:17:37 -0300

    feat: adicionada consulta de dados de energia por prédio e data e suporte a paginação

--- src/main/java/br/com/inovatech/powerguard/repositories/EnergyRepository.java
+++ src/main/java/br/com/inovatech/powerguard/repositories/EnergyRepository.java
@@ -1,12 +1,18 @@
 package br.com.inovatech.powerguard.repositories;
 
 import br.com.inovatech.powerguard.domains.EnergyDomain;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
-public interface EnergyRepository extends ReactiveMongoRepository<EnergyDomain, Integer> {
+import java.time.LocalDateTime;
 
-    @Query("{ 'building': ?0 }")
-    Flux<EnergyDomain> findByBuilding(String building);
+public interface EnergyRepository extends ReactiveMongoRepository<EnergyDomain, String> {
+
+    @Query("{ 'building': ?0, 'createdAt': { $gte: ?1, $lt: ?2 } }")
+    Flux<EnergyDomain> findByBuildingAndToday(String building, LocalDateTime startOfDay, LocalDateTime endOfDay);
+
+    @Query("{ }")
+    Flux<EnergyDomain> findAll(Pageable pageable);
 }
