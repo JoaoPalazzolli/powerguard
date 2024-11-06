3ba03aaac63b0be220645d9da3dac12228c8404f
João Pedro <joaopedropalazzolli@gmail.com>
2024-11-05 23:36:06 -0300

    feat: adicionar a classe de exceção ExternalApiResponseException com status HTTP 400

---
+++ src/main/java/br/com/inovatech/powerguard/infra/exceptions/ExternalApiResponseException.java
@@ -0,0 +1,13 @@
+package br.com.inovatech.powerguard.infra.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ExternalApiResponseException extends RuntimeException{
+
+    public ExternalApiResponseException(String msg) {
+        super(msg);
+    }
+
+}
